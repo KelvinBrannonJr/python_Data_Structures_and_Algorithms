56064ec175af653bdccb4ea8862e1aa7044de16a
Kelvin <Kelzjr56@gmail.com>
2023-07-08 14:13:13 -0400

    Added append ad print function for linked list

--- linked_list.py
+++ linked_list.py
@@ -11,9 +11,37 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append_node(self, value):
+        add_node = Node(value)
+        if self.length == 0:
+            self.head = add_node
+            self.tail = add_node
+
+        else:
+            self.tail.next = add_node
+            self.tail = add_node
+        self.length += 1
+        return True
+
 
 my_linked_list = LinkedList(4)
+
 
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
+
+my_linked_list = LinkedList(1)
+
+my_linked_list.append_node(2)
+
+my_linked_list.print_list()
+
+
+
