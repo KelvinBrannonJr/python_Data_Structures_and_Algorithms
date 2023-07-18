044ccb42dc6b9d1d12bec3fe15dbed522566ceab
Kelvin <Kelzjr56@gmail.com>
2023-07-18 14:36:05 -0400

    Added enqueue and dequeue methods for queue

--- stack_and_queue.py
+++ stack_and_queue.py
@@ -37,11 +37,58 @@ class Stack:
             temp = temp.next
 
 
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        return True
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+# Stack object instantiation
+print("Stack Objects")
 my_stack = Stack(7)
 my_stack.push(23)
-my_stack.push(3)
-my_stack.push(11)
-
-print(my_stack.pop(), '\n')
-
 my_stack.print_stack()
+
+
+# Queue object instantiation
+print("Queue Objects")
+my_queue = Queue(1)
+my_queue.enqueue(2)
+
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+
