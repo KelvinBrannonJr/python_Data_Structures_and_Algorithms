77093d3bf4eb4dbf013ae3b517e8698c6d02eef7
Kelvin <Kelzjr56@gmail.com>
2023-07-25 10:54:56 -0400

    Added merge_sort file and merge method

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,5 @@
 def bubble_sort(my_list):
-    for i in range(len(my_list) -1, 0, -1):
+    for i in range(len(my_list) - 1, 0, -1):
         for j in range(i):
             if my_list[j] > my_list[j + 1]:
                 temp = my_list[j]
@@ -11,7 +11,7 @@ def bubble_sort(my_list):
 def selection_sort(my_list):
     for i in range(len(my_list) - 1):
         min_index = i
-        for j in range(i+1, len(my_list)):
+        for j in range(i + 1, len(my_list)):
             if my_list[j] < my_list[min_index]:
                 min_index = j
         temp = my_list[i]
