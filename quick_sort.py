1584087c60d4c56ec49db28dcf2b9113dffe69fe
Kelvin <Kelzjr56@gmail.com>
2023-07-25 11:52:12 -0400

    Added quick_sort_helper and quick_sort methods

--- quick_sort.py
+++ quick_sort.py
@@ -15,9 +15,19 @@ def pivot(my_list, pivot_index, end_index):
     return swap_index
 
 
-my_list = [4, 6, 1, 7, 3, 2, 5]
+def quick_sort_helper(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index - 1)
+        quick_sort_helper(my_list, pivot_index+1, right)
 
-print(pivot(my_list, 0, 6))
+    return my_list
 
-print(my_list)
+
+def quick_sort(my_list):
+    return quick_sort_helper(my_list, 0, len(my_list)-1)
+
+
+print(quick_sort([4, 6, 1, 7, 3, 2, 5]))
+
 
